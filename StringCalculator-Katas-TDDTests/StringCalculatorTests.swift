4535afe4b52c28cf6ec8dc665014d0f6a8d58e17
Jose <jose@medici.md>
2020-09-10 16:34:15 -0400

    Refactor custom delimiter logic

--- StringCalculator-Katas-TDDTests/StringCalculatorTests.swift
+++ StringCalculator-Katas-TDDTests/StringCalculatorTests.swift
@@ -22,17 +22,8 @@ struct StringCalculator {
     }
 
     static func add(_ input: String) throws -> Int {
-        var customDelimiter: String?
-        var parsedInput = input
-        
-        if input.hasPrefix("//"), let newLineIndex = input.firstIndex(where: { $0 == "\n" }) {
-            let customDelimiterStartIndex = input.index(input.startIndex, offsetBy: 2)
-            let customDelimiterIndexRange = customDelimiterStartIndex..<newLineIndex
-            customDelimiter = String(input[customDelimiterIndexRange])
-            
-            let customDelimiterDefinitionRange = input.startIndex...newLineIndex
-            parsedInput.removeSubrange(customDelimiterDefinitionRange)
-        }
+        let customDelimiter = getCustomDelimeterIfPresent(input)
+        let parsedInput = cleanupInputIfNeeded(input)
         
         let operands = parsedInput.split(whereSeparator: { separatorRule($0, customDelimiter: customDelimiter) })
             .compactMap({ Int($0) })
@@ -43,6 +34,27 @@ struct StringCalculator {
             guard e <= 1000 else { return }
             acc += e
         })
+    }
+    
+    private static func getCustomDelimeterIfPresent(_ input: String) -> String? {
+        guard input.hasPrefix("//"), let newLineIndex = input.firstIndex(where: { $0 == "\n" }) else {
+            return nil
+        }
+        
+        let customDelimiterStartIndex = input.index(input.startIndex, offsetBy: 2)
+        let customDelimiterIndexRange = customDelimiterStartIndex..<newLineIndex
+        return String(input[customDelimiterIndexRange])
+    }
+    
+    private static func cleanupInputIfNeeded(_ input: String) -> String {
+        guard input.hasPrefix("//"), let newLineIndex = input.firstIndex(where: { $0 == "\n" }) else {
+            return input
+        }
+        
+        var inputCleaned = input
+        let customDelimiterDefinitionRange = input.startIndex...newLineIndex
+        inputCleaned.removeSubrange(customDelimiterDefinitionRange)
+        return inputCleaned
     }
     
     private static func separatorRule(_ character: Character, customDelimiter: String?) -> Bool {
