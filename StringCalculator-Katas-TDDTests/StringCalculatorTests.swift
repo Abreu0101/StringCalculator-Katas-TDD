b00e96072037d7f7cb43bde705e42b8521e79e8a
Jose <jose@medici.md>
2020-09-10 15:59:08 -0400

    Test add with negative input throws an exception

--- StringCalculator-Katas-TDDTests/StringCalculatorTests.swift
+++ StringCalculator-Katas-TDDTests/StringCalculatorTests.swift
@@ -9,11 +9,31 @@
 import XCTest
 
 struct StringCalculator {
-    
-    static func add(_ input: String) -> Int {
-        return input.split(whereSeparator: separatorRule)
+        
+    enum Error: Swift.Error, Equatable {
+        case negativesNotAllowed(message: String)
+        
+        static func ==(lhs: Error, rhs: Error) -> Bool {
+            switch (lhs, rhs) {
+            case let (.negativesNotAllowed(lhsMessage), .negativesNotAllowed(rhsMessage)):
+                return lhsMessage == rhsMessage
+            }
+        }
+    }
+
+    static func add(_ input: String) throws -> Int {
+        let operands = input.split(whereSeparator: separatorRule)
             .compactMap({ Int($0) })
-            .reduce(into: 0, { acc, e in acc += e })
+        
+        let negativeNumbers = operands.filter({ $0 < 0 })
+        if !negativeNumbers.isEmpty {
+            let negativeNumbersCommandSeparated = negativeNumbers.map({ "\($0)," })
+                .joined()
+                .dropLast(1)
+            throw Error.negativesNotAllowed(message: "negatives not allowed: \(negativeNumbersCommandSeparated)")
+        }
+        
+        return operands.reduce(into: 0, { acc, e in acc += e })
     }
     
     private static func separatorRule(_ character: Character) -> Bool {
@@ -24,29 +44,40 @@ struct StringCalculator {
 
 class StringCalculatorTests: XCTestCase {
 
-    func test_add_returnsZeroForEmptyInput() {
-        let receivedSum = StringCalculator.add("")
+    func test_add_returnsZeroForEmptyInput() throws {
+        let receivedSum = try StringCalculator.add("")
         XCTAssertEqual(receivedSum, 0)
     }
 
-    func test_add_withSingleNumberInputReturnsInput() {
-        let receivedSum = StringCalculator.add("2")
+    func test_add_withSingleNumberInputReturnsInput() throws {
+        let receivedSum = try StringCalculator.add("2")
         XCTAssertEqual(receivedSum, 2)
     }
     
-    func test_add_withTwoNumbersCommaDelimitedReturnsTheSum() {
-        let receivedSum = StringCalculator.add("10,20")
+    func test_add_withTwoNumbersCommaDelimitedReturnsTheSum() throws {
+        let receivedSum = try StringCalculator.add("10,20")
         XCTAssertEqual(receivedSum, 30)
     }
     
-    func test_add_withTwoNumbersNewlineDelimitedReturnsTheSum() {
-        let receivedSum = StringCalculator.add("2\n3")
+    func test_add_withTwoNumbersNewlineDelimitedReturnsTheSum() throws {
+        let receivedSum = try StringCalculator.add("2\n3")
         XCTAssertEqual(receivedSum, 5)
     }
     
-    func test_add_withMultiplesNumbersMixedDelimitedReturnsTheSum() {
-        let receivedSum = StringCalculator.add("1\n2,3\n4")
+    func test_add_withMultiplesNumbersMixedDelimitedReturnsTheSum() throws {
+        let receivedSum = try StringCalculator.add("1\n2,3\n4")
         XCTAssertEqual(receivedSum, 10)
+    }
+    
+    func test_add_inputWithNegativeNumbersThrowAnException() {
+        XCTAssertThrowsError(try StringCalculator.add("-1,2,-3")) { receivedError in
+            switch receivedError {
+            case let error as StringCalculator.Error:
+                XCTAssertEqual(error, .negativesNotAllowed(message: "negatives not allowed: -1,-3"))
+            default:
+                XCTFail("Expected to received negativesNotAllowed, but got \(receivedError) instead")
+            }
+        }
     }
     
 }
