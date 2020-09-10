fabf358008d4eeb25257f60b852ff7cbc1c998db
Jose <jose@medici.md>
2020-09-10 14:43:42 -0400

    Derive StringCalculator model from test init

---
+++ StringCalculator-Katas-TDDTests/StringCalculatorTests.swift
@@ -0,0 +1,21 @@
+//
+//  StringCalculatorTests.swift
+//  StringCalculator-Katas-TDDTests
+//
+//  Created by Roberto Abreu on 9/10/20.
+//  Copyright © 2020 Roberto Abreu. All rights reserved.
+//
+
+import XCTest
+
+struct StringCalculator {
+    
+}
+
+class StringCalculatorTests: XCTestCase {
+
+    func test_init() {
+        _ = StringCalculator()
+    }
+
+}
